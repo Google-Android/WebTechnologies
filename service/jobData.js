fed40ba540eb409b3437e2cf55dd0c7e45d1f349
Yipingcherisheverymoment <yzhao73@sheffield.ac.uk>
2019-05-12 16:00:43 +0100

    add the method of secondary search job in the jobData.js

--- service/jobData.js
+++ service/jobData.js
@@ -1,5 +1,4 @@
 
-
 var jobData= {
     searchJob: function (condition, condition2, callback) {
         require('../mongoDB/tools/connection');
@@ -7,10 +6,15 @@ var jobData= {
         JobModel.find({
                 $and: [
                     {
-                        $or: [{title: {$regex: condition}}, {companyName: {$regex: condition}}, {jobType: {$regex: condition}}]
+                        $or: [{title: {$regex: condition, $options: 'i'}}, {
+                            companyName: {
+                                $regex: condition,
+                                $options: 'i'
+                            }
+                        }, {jobType: {$regex: condition, $options: 'i'}}]
                     },
                     {
-                        $or: [{city: {$regex: condition2}}, {postCode: {$regex: condition2}}]
+                        $or: [{city: {$regex: condition2, $options: 'i'}}, {postCode: {$regex: condition2, $options: 'i'}}]
                     }
                 ]
             },
@@ -18,27 +22,106 @@ var jobData= {
             function (err, result) {
                 if (err) throw err;
                 callback(null, result);
-            });
+            }).sort({postDate:-1,title:1});
+
+    },
+
+    secondarySearchJob: function (keyword, location, jType, sal1,sal2, callback) {
+        require('../mongoDB/tools/connection');
+        var JobModel = require('../mongoDB/models/job');
+        if(sal2==null){
+            JobModel.find({
+                $and: [
+                    {
+                        $or: [{title: {$regex: keyword, $options: 'i'}}, {
+                            companyName: {$regex: keyword,$options: 'i'}
+                        }]
+                    },
+                    {
+                        $or: [{city: {$regex: location, $options: 'i'}}, {postCode: {$regex: location, $options: 'i'}}]
+                    }, {
+                        jobType: {$regex: jType, $options: 'i'}
+                    }, {
+                        "salary.year": {$gte: sal1}
+                    }
+                ]},function (err, result) {
+                if (err) throw err;
+                callback(null, result);
+            }).sort({postDate:-1,title:1});
+
+
+        }else {
+            JobModel.find({
+                    $and: [
+                        {
+                            $or: [{title: {$regex: keyword, $options: 'i'}}, {
+                                companyName: {
+                                    $regex: keyword,
+                                    $options: 'i'
+                                }
+                            }]
+                        },
+                        {
+                            $or: [{city: {$regex: location, $options: 'i'}}, {
+                                postCode: {
+                                    $regex: location,
+                                    $options: 'i'
+                                }
+                            }]
+                        }, {
+                            jobType: {$regex: jType, $options: 'i'}
+                        }, {
+                            "salary.year": {$gte: sal1, $lte: sal2}
+                        }
+                    ]
+                },
+
+                function (err, result) {
+                    if (err) throw err;
+                    callback(null, result);
+                }).sort({postDate:-1,title:1});
+        }
 
     },
 
     showJobDetails: function (company, callback) {
         require('../mongoDB/tools/connection');
         var JobModel = require('../mongoDB/models/job');
-        JobModel.find({companyName:company},
+        JobModel.find({companyName: company},
             function (err, result) {
                 if (err) throw err;
                 callback(null, result);
             });
 
     }
-}
+};
 
 module.exports=jobData;   // export this module
 
-// jobData.showJobDetails("Urban Outfitters Inc.",function(err,doc){
+
+
+
+
+
+// jobData.showJobDetails("urban outfitters",function(err,doc){
 //     if(!err){
 //         console.log(doc);
 //     }
 // })
+
+// var a="amazon";
+// var b="s1";
+
+//console.log(role_query);
+//keyword, location, jType, sal1,sal2, callback
+// jobData.searchJob(a,b,function(err,docs){
+//     if(!err){
+//         console.log(docs);
+//     }
+// });
+// jobData.secondarySearchJob(a,b,"Part-Time",30000,null,function(err,docs){
+//     if(!err){
+//         console.log(docs);
+//     }
+// });
 
