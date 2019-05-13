18c74021a0209fb857f92b9289fe47c3851c27de
Shunbao <baoyesrtyle@gmail.com>
2019-05-13 18:33:33 +0100

    header.ejs  添加密码重复性验证

--- public/javascripts/headerJS.js
+++ public/javascripts/headerJS.js
@@ -1,15 +1,15 @@
 /**
  * all function here are used for components in header page.
  */
-$(document).ready(function() {
+$(document).ready(function () {
 
     /**
      * get data from login form and validate the username and password.
      */
-    $('#login').on('submit', function(event) {
+    $('#login').on('submit', function (event) {
         event.preventDefault();
 
-        var loginInfo={};
+        var loginInfo = {};
         loginInfo['loginEmail'] = $('#loginEmail1').val();
         loginInfo['loginPassword'] = $('#loginPassword').val();
 
@@ -19,12 +19,12 @@ $(document).ready(function() {
             type: 'POST',
             url: '/login',
             data: loginInfo,
-            success: function(data) {
+            success: function (data) {
                 // alert('ajax successfully.')
                 // alert("error:"+data.errInfo);
                 //do something with the data via front-end framework
-                if(data.errInfo==1){
-                    $('#errorNotification').show();
+                if (data.errInfo == 1) {
+                    $('#invalidUsernamePasswordNotification').show();
                 } else {
                     window.location.reload();
                     // window.location.href='/';
@@ -38,19 +38,19 @@ $(document).ready(function() {
 
 
     /**
-     * register for a new customer user
-     * validate the username and password; if not existed yet, add the new user. 
+     * register for a new consumer user
+     * validate the username and password; if not existed yet, add the new user.
      */
-    $('#consumerRegister').on('submit', function(event) {
+    $('#consumerRegister').on('submit', function (event) {
         // alert('get register data done.');
         event.preventDefault();
 
-        var d={};
+        var d = {};
 
         // [{name: "a1", value: "xx"},{name: "a2", value: "xx"}],
         var wholeForm = $('#consumerRegister').serializeArray();
 
-        $.each(wholeForm, function() {
+        $.each(wholeForm, function () {
             d[this.name] = this.value;
         });
 
@@ -60,36 +60,34 @@ $(document).ready(function() {
             type: 'POST',
             url: '/register',
             data: d,
-            success: function(data) {
-                if(data.result===0){
-                    alert("Unfortunately, the email address you've entered already exists. ");
-                } else if(data.result === 1){
-                    window.location.href='/';
-                } else if(data.result === 2){
+            success: function (data) {
+                if (data.result === 0) {
+                    $('#duplicateConsumerEmailNotification').show();
+                } else if (data.result === 1) {
+                    window.location.href = '/';
+                } else if (data.result === 2) {
                     alert("Sorry, cannot create a new account now.");
                 }
             }
         });
         return false;
     });
-
-
 
 
     /**
      * register for a new company user
-     * validate the username and password; if not existed yet, add the new user. 
+     * validate the username and password; if not existed yet, add the new user.
      */
-    $('#companyRegister').on('submit', function(event) {
+    $('#companyRegister').on('submit', function (event) {
         // alert('get company register data done.');
         event.preventDefault();
 
-        var d={};
+        var d = {};
 
         // [{name: "a1", value: "xx"},{name: "a2", value: "xx"}],
         var wholeForm = $('#companyRegister').serializeArray();
 
-        $.each(wholeForm, function() {
+        $.each(wholeForm, function () {
             d[this.name] = this.value;
         });
 
@@ -99,12 +97,12 @@ $(document).ready(function() {
             type: 'POST',
             url: '/register',
             data: d,
-            success: function(data) {
-                if(data.result===0){
-                    alert("Unfortunately, the email address you've entered already exists. ");
-                } else if(data.result === 1){
-                    window.location.href='/';
-                } else if(data.result === 2){
+            success: function (data) {
+                if (data.result === 0) {
+                    $('#duplicateCompanyEmailNotification').show();
+                } else if (data.result === 1) {
+                    window.location.href = '/';
+                } else if (data.result === 2) {
                     alert("Sorry, cannot create a new account now.");
                 }
             }
@@ -113,6 +111,32 @@ $(document).ready(function() {
     });
 
 
-
 });
+/**
+ * show notification when the two passwords entered by consumers are not consistent
+ */
+var consumerPasswordConfirmation = function() {
+    if (document.getElementById('consumerPassword').value ==
+        document.getElementById('consumerRepeatPassword').value) {
+        $('#consumerPasswordNotMatching').hide();
+
+    } else {
+        $('#consumerPasswordNotMatching').show();
+
+    }
+}
+
+/**
+ * show notification when the two passwords entered by companies are not consistent
+ */
+var companyPasswordConfirmation = function() {
+    if (document.getElementById('companyPassword').value ==
+        document.getElementById('companyRepeatPassword').value) {
+        $('#companyPasswordNotMatching').hide();
+
+    } else {
+        $('#companyPasswordNotMatching').show();
+
+    }
+}
 
