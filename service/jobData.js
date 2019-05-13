3a4eb8b6ba7641b71d5f9a3022711885dbd89e38
Yipingcherisheverymoment <yzhao73@sheffield.ac.uk>
2019-05-13 10:49:21 +0100

    add the method of secondary search job in the jobData.js

--- service/jobData.js
+++ service/jobData.js
@@ -6,15 +6,12 @@ var jobData= {
         JobModel.find({
                 $and: [
                     {
-                        $or: [{title: {$regex: condition, $options: 'i'}}, {
-                            companyName: {
-                                $regex: condition,
-                                $options: 'i'
-                            }
-                        }, {jobType: {$regex: condition, $options: 'i'}}]
+                        $or: [{title: {$regex: condition, $options: 'i'}},
+                              {companyName: {$regex: condition, $options: 'i'}},
+                              {jobType: {$regex: condition, $options: 'i'}}]
                     },
                     {
-                        $or: [{city: {$regex: condition2, $options: 'i'}}, {postCode: {$regex: condition2, $options: 'i'}}]
+                        $or: [{city: {$regex: condition2, $options: 'i'}}, {postcode: {$regex: condition2, $options: 'i'}}]
                     }
                 ]
             },
@@ -26,23 +23,60 @@ var jobData= {
 
     },
 
-    secondarySearchJob: function (keyword, location, jType, sal1,sal2, callback) {
+    // job: function (condition, condition2, callback) {
+    //     require('../mongoDB/tools/connection');
+    //     var JobModel = require('../mongoDB/models/job');
+    //
+    //     for(int i=0;i<condition.length;i++){
+    //
+    //     }
+    //
+    //     var _filter={
+    //         $and: [
+    //
+    //         {
+    //             $or: [{title: {$regex: condition[i], $options: 'i'}},
+    //                 {companyName: {$regex: condition[i], $options: 'i'}},
+    //                 {jobType: {$regex: condition[i], $options: 'i'}}]
+    //         }
+    //
+    //             },
+    //             {
+    //                 $or: [{city: {$regex: condition2, $options: 'i'}}, {postcode: {$regex: condition2, $options: 'i'}}]
+    //             }
+    //         ]
+    //
+    //     },
+    //
+    //     JobModel.find(_filter,function (err, result) {
+    //             if (err) throw err;
+    //             callback(null, result);
+    //         }).sort({postDate:-1,title:1}).forEach(
+    //     function(item) { item.forEach(
+    //
+    // );
+    //
+    // },
+
+    secondarySearchJob: function (keyword, location, jType, sal1,sal2, jobIndustry,callback) {
         require('../mongoDB/tools/connection');
         var JobModel = require('../mongoDB/models/job');
         if(sal2==null){
             JobModel.find({
                 $and: [
                     {
-                        $or: [{title: {$regex: keyword, $options: 'i'}}, {
-                            companyName: {$regex: keyword,$options: 'i'}
-                        }]
+                        $or: [{title: {$regex: keyword, $options: 'i'}},
+                            {companyName: {$regex: keyword,$options: 'i'}},
+                            {jobType: {$regex: keyword, $options: 'i'}}]
                     },
                     {
-                        $or: [{city: {$regex: location, $options: 'i'}}, {postCode: {$regex: location, $options: 'i'}}]
+                        $or: [{city: {$regex: location, $options: 'i'}}, {postcode: {$regex: location, $options: 'i'}}]
                     }, {
                         jobType: {$regex: jType, $options: 'i'}
                     }, {
-                        "salary.year": {$gte: sal1}
+                        salary: {$gte: sal1}
+                    },{
+                        industry: {$regex:jobIndustry, $options: 'i'}
                     }
                 ]},function (err, result) {
                 if (err) throw err;
@@ -63,15 +97,17 @@ var jobData= {
                         },
                         {
                             $or: [{city: {$regex: location, $options: 'i'}}, {
-                                postCode: {
+                                postcode: {
                                     $regex: location,
                                     $options: 'i'
                                 }
                             }]
                         }, {
                             jobType: {$regex: jType, $options: 'i'}
-                        }, {
-                            "salary.year": {$gte: sal1, $lte: sal2}
+                        },{
+                            salary: {$gte: sal1, $lte: sal2}
+                        },{
+                            industry: {$regex:jobIndustry, $options: 'i'}
                         }
                     ]
                 },
@@ -84,16 +120,41 @@ var jobData= {
 
     },
 
-    showJobDetails: function (company, callback) {
+    showJobDetails: function (id, callback) {
         require('../mongoDB/tools/connection');
         var JobModel = require('../mongoDB/models/job');
-        JobModel.find({companyName: company},
+        JobModel.find({_id: id},
             function (err, result) {
                 if (err) throw err;
                 callback(null, result);
             });
 
+    },
+
+    postJob: function(jobName,company,jobIndustry,type,sal,picUrl,details,date,adStreet,adCity,adState,zipcode,adCoun,callback){
+        require('../mongoDB/tools/connection');
+        var JobModel = require('../mongoDB/models/job');
+        JobModel.create({
+            title: jobName,
+            companyName: company,
+            industry: jobIndustry,
+            jobType: type,
+            city: adCity,
+            postCode: zipcode,
+            salary: sal,
+            picture: picUrl,
+            description: details,
+            postDate: date,
+            street: adStreet,
+            state: adState,
+            country: adCoun
+        },
+            function (err, result) {
+                if (err) throw err;
+                callback(null, result);
+            });
     }
+
 };
 
 module.exports=jobData;   // export this module
@@ -102,6 +163,12 @@ module.exports=jobData;   // export this module
 
 
 
+// jobData.postJob("dd","d","f",'e',"22",333,"London","11.jpg",
+//     "f","12-01",function(err,docs){
+//     if(!err){
+//         console.log(docs);
+//     }
+//     })
 
 // jobData.showJobDetails("urban outfitters",function(err,doc){
 //     if(!err){
@@ -109,17 +176,28 @@ module.exports=jobData;   // export this module
 //     }
 // })
 
-// var a="amazon";
-// var b="s1";
+var a="amazon part-time";
+var b="s1";
+
+
+var keyArr=a.split(" ");
+
+var key_query="";
+for(var i=0;i<keyArr.length;i++){
+    key_query=key_query+keyArr+"|";
+}
+key_query=key_query.substring(key_query.length);
+
+let condition=new RegExp(key_query);
 
 //console.log(role_query);
 //keyword, location, jType, sal1,sal2, callback
-// jobData.searchJob(a,b,function(err,docs){
-//     if(!err){
-//         console.log(docs);
-//     }
-// });
-// jobData.secondarySearchJob(a,b,"Part-Time",30000,null,function(err,docs){
+jobData.searchJob(condition,b,function(err,docs){
+    if(!err){
+        console.log(docs);
+    }
+});
+// jobData.secondarySearchJob(a,b,"Part-Time",0,null,"nanny",function(err,docs){
 //     if(!err){
 //         console.log(docs);
 //     }
