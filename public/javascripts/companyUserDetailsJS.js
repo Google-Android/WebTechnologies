b3796eb2367e54c93beb1c7783c9d03808917a0e
Shunbao <baoyesrtyle@gmail.com>
2019-05-13 19:32:48 +0100

    consumerUserDetails.ejs companyUserDetails.ejs  consumerUserDetailsJS.js companyUserDetailsJS.js
     修复密码不一致也能提交的bug

--- public/javascripts/companyUserDetailsJS.js
+++ public/javascripts/companyUserDetailsJS.js
@@ -2,12 +2,14 @@
  * show notification when the two passwords entered by consumers are not consistent in account management.
  */
 var companyManagementPasswordConfirmation = function() {
+    var check;
     if (document.getElementById('companyManagementPassword').value ==
         document.getElementById('companyManagementRepeatPassword').value) {
         $('#companyManagementPasswordNotMatching').hide();
-
+        check = true;
     } else {
         $('#companyManagementPasswordNotMatching').show();
-
+        check = false;
     }
+    return check;
 }
--- public/javascripts/consumerUserDetailsJS.js
+++ public/javascripts/consumerUserDetailsJS.js
@@ -1,13 +1,15 @@
 /**
  * show notification when the two passwords entered by consumers are not consistent in account management.
  */
-var consumerManagementPasswordConfirmation = function() {
+var consumerManagementPasswordConfirmation = function () {
+    var check = false;
     if (document.getElementById('consumerManagementPassword').value ==
         document.getElementById('consumerManagementRepeatPassword').value) {
         $('#consumerManagementPasswordNotMatching').hide();
-
+        check = true;
     } else {
         $('#consumerManagementPasswordNotMatching').show();
-
+        check = false;
     }
+    return check;
 }
