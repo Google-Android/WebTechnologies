9044647766e793a98fcd6fd037216c048346c0c4
crystalQin <qinfanhua2015@163.com>
2019-05-15 00:03:48 +0100

    finish first search cv part.

--- routes/findCvs.js
+++ routes/findCvs.js
@@ -4,6 +4,7 @@
  */
 var express = require('express');
 var router = express.Router();
+var findCvData = require('../service/findCvData');
 
 
 /*  */
@@ -14,7 +15,32 @@ router.get('/', function(req, res) {
 
 router.post('/', function(req, res) {
     console.log('***findCvs***post***');
-    res.render('findCvs',{user:req.session.user});
+
+    var operation = req.body.operation=null?"":req.body.operation;
+
+    if(operation == 'firstSearch'){
+      var cvKeyword = req.body.cvKeyword=null?"":req.body.cvKeyword;
+      var cvLocation = req.body.cvLocation=null?"":req.body.cvLocation;
+      console.log('operation:'+operation+",cvKeyword:"+cvKeyword+",cvLocation:"+cvLocation);
+
+      findCvData.searchCv(cvKeyword,cvLocation,function(err, cvs){
+        if(err){
+          console.log('err:'+err);
+          throw err;
+        }
+        if(cvs){
+          if(cvs.length == 0){
+            console.log('cannot find cv according to the conditions.');
+            res.json({'result':2});
+          } else {
+            console.log('find cv length:'+cvs.length);
+            res.json({'result':1,'cvs':cvs});
+          } 
+        }
+      });
+    }
+
+    
 });
 
 
