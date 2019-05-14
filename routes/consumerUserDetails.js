81c34d1ebd70ccbe409d5be60b3c0853d0776162
crystalQin <qinfanhua2015@163.com>
2019-05-14 21:53:39 +0100

    finish customerUserDetails page.

--- routes/consumerUserDetails.js
+++ routes/consumerUserDetails.js
@@ -3,42 +3,68 @@ var router = express.Router();
 var reviewData = require('../service/reviewData');
 var cvData = require('../service/cvData');
 var handleData = require('../mongoDB/handleData');
+var jobData = require('../service/jobData');
 
-/* GET index page after login. */
+/* GET user Details page. */
 router.get('/', function(req, res, next) {
   console.log('***consumerUserDetails***get***');
 
-  var userName = req.query.userName;
-  console.log('userId:'+userName);
-
-  //find reviews to this user by searching userName in database
-  reviewData.searchReview(userName,function (err, reviews) {
-    if(err){
-      console.log('err:'+err);
-      throw err;
-    }
-    if(reviews){// exists reviews for this user
-      console.log('reviews size:'+reviews.length);
-    }
-
-    var cvId = req.session.user.cv;
-    cvData.showCv(cvId,function(err, cv){
+  var operation = req.query.operation==null?"":req.query.operation;
+  if(operation == 'deleteJob'){
+    var jobId = req.query.jobId;
+    console.log('jobId:'+jobId);
+    jobData.deleteJob(jobId, function(err,job){
       if(err){
         console.log('err:'+err);
         throw err;
       }
-      if(cv){ // find cv of this user
-        console.log('find cv:'+cv);
+      if(job){// delete job
+        console.log('delete job:'+job);
       }
-      console.log('reviews:'+reviews+",cv:"+cv);
-      res.render('consumerUserDetails', { user:req.session.user,'reviews':reviews,'cv':cv});
+      res.redirect('/consumerUserDetails');
     });
-
-  });
-
+  } else {
+    var userName = req.session.user.name;
+    console.log('userId:'+userName);
   
-
+    //find reviews to this user by searching userName in database
+    reviewData.searchReview(userName,function (err, reviews) {
+      if(err){
+        console.log('err:'+err);
+        throw err;
+      }
+      if(reviews){// exists reviews for this user
+        console.log('reviews size:'+reviews.length);
+      }
   
+      if(req.session.user.personOrComp == 'p'){ // consumer user
+        var cvId = req.session.user.cv;
+        cvData.showCv(cvId,function(err, cv){
+          if(err){
+            console.log('err:'+err);
+            throw err;
+          }
+          if(cv){ // find cv of this user
+            console.log('find cv:'+cv);
+          }
+          console.log('reviews:'+reviews+",cv:"+cv);
+          res.render('consumerUserDetails', { user:req.session.user,'reviews':reviews,'cv':cv});
+        });
+      } else if(req.session.user.personOrComp == 'c'){ // company user
+          jobData.showAllJobsByCompanyName(userName, function(err, jobs){
+            if(err){
+              console.log('err:'+err);
+              throw err;
+            }
+            if(jobs){ // find all jobs posted by this company
+              console.log('find jobs:'+jobs);
+            }
+            console.log('reviews:'+reviews+",jobs:"+jobs);
+            res.render('consumerUserDetails', { user:req.session.user,'reviews':reviews,'jobs':jobs});
+          });
+      }
+    });
+  }
 });
 
 
