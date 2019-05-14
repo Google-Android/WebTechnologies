858e7487266c545b40f1a06bcbecb2733a01b450
crystalQin <qinfanhua2015@163.com>
2019-05-14 14:30:23 +0100

    finish logout part.

--- routes/index.js
+++ routes/index.js
@@ -9,11 +9,22 @@ var router = express.Router();
 /* GET index page. */
 router.get('/', function(req, res) {
   console.log('***index***get***');
-  //if user has been stored in session, the username will be shown on web page.
-  if(req.session.user){
-    res.render('index',{user:req.session.user});
-  } else{
-    res.render('index',{user:null});
+  var operation = req.query.operation==null?"":req.query.operation;
+  if(operation == ""){
+      //if user has been stored in session, the username will be shown on web page.
+      if(req.session.user){
+          res.render('index',{user:req.session.user});
+      } else{
+          res.render('index',{user:null});
+      }
+  } else if(operation == "logout"){
+      //when processing logout, the session will be destroyed.
+      req.session.destroy(function (err) {
+          if(err){
+            console.log('err:'+err);
+          }
+          res.redirect('/');
+      })
   }
 });
 
