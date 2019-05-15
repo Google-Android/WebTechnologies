721fa491ca4558ff4b1cb3c3c8ddee37a85ea577
Shunbao <baoyesrtyle@gmail.com>
2019-05-15 22:37:03 +0100

    * add latitude and longitude for jobRequests.ejs and postJobs.ejs * update autoCompleteAddress.ejs

--- public/javascripts/addressAutocompleteJS.js
+++ public/javascripts/addressAutocompleteJS.js
@@ -1,9 +1,9 @@
 /**
- * Autocomplete address by using google map javascript API
+ * autofill the form for the users by using Google map API
+ * @Author Shunbao Li
+ * @last_modify_date     15/05/2019
+ * @type {{country: string, route: string, postal_town: string, administrative_area_level_1: string, postal_code: string}}
  */
-
-var placeSearch, autocomplete;
-
 var componentForm = {
     route: 'long_name',
     postal_town: 'long_name',
@@ -13,62 +13,30 @@ var componentForm = {
 };
 
 /**
- *
+ * search for the place input by users and autofill in the searching results into specific place
  */
-function initAutocomplete() {
-    // Create the autocomplete object, restricting the search predictions to
-    // geographical location types.
-    autocomplete = new google.maps.places.Autocomplete(
-        document.getElementById('autocomplete'), {types: ['geocode']});
+function autoFillAddress() {
+    var input = document.getElementById('searchTextField');
+    var autocomplete = new google.maps.places.Autocomplete(input);
+    google.maps.event.addListener(autocomplete, 'place_changed', function () {
+        // Get the place details from the autocomplete object.
+        var place = autocomplete.getPlace();
 
-    // Avoid paying for data that you don't need by restricting the set of
-    // place fields that are returned to just the address components.
-    autocomplete.setFields(['address_component']);
-
-    // When the user selects an address from the drop-down, populate the
-    // address fields in the form.
-    autocomplete.addListener('place_changed', fillInAddress);
-}
-
-
-/**
- *
- */
-function fillInAddress() {
-    // Get the place details from the autocomplete object.
-    var place = autocomplete.getPlace();
-
-    for (var component in componentForm) {
-        document.getElementById(component).value = '';
-        document.getElementById(component).disabled = false;
-    }
-
-    // Get each component of the address from the place details,
-    // and then fill-in the corresponding field on the form.
-    for (var i = 0; i < place.address_components.length; i++) {
-        var addressType = place.address_components[i].types[0];
-        if (componentForm[addressType]) {
-            var val = place.address_components[i][componentForm[addressType]];
-            document.getElementById(addressType).value = val;
+        //fill in latitude and longitude into hidden input boxes
+        document.getElementById('cityLat').value = place.geometry.location.lat();
+        document.getElementById('cityLng').value = place.geometry.location.lng();
+        for (var component in componentForm) {
+            document.getElementById(component).value = '';
         }
-    }
-}
 
-// Bias the autocomplete object to the user's geographical location,
-// as supplied by the browser's 'navigator.geolocation' object.
-/**
- *
- */
-function geolocate() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            var geolocation = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
-            var circle = new google.maps.Circle(
-                {center: geolocation, radius: position.coords.accuracy});
-            autocomplete.setBounds(circle.getBounds());
-        });
-    }
+        // Get each component of the address from the place details,
+        // and then fill-in the corresponding field on the form.
+        for (var i = 0; i < place.address_components.length; i++) {
+            var addressType = place.address_components[i].types[0];
+            if (componentForm[addressType]) {
+                var val = place.address_components[i][componentForm[addressType]];
+                document.getElementById(addressType).value = val;
+            }
+        }
+    });
 }
