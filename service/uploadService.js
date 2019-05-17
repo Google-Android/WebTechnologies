040334060937a67467c203e9acf6d73c5d68cc12
acp17fq <fqin2@sheffield.ac.uk>
2019-05-17 10:31:46 +0100

    delete mkdir public image folder.

--- service/uploadService.js
+++ service/uploadService.js
@@ -2,26 +2,29 @@
  * This file is used for uploading service.
  */
 // use fs to create or access folder
-var fs = require('fs');
+// var fs = require('fs');
+
 // use multer to upload file
 var multer = require('multer');
-
-var currentTime = Date.now();
 
 
 var uploadService={
 
     uploadFile:function (currentTime) {
 
-        try{
-            fs.accessSync('./public/image/');
-        }catch(e){
-            fs.mkdirSync('./public/image/');
-        }
+        uploadPath = './public/image/';
+
+        // try{
+        //     // fs.accessSync('./public/image/');
+        //     fs.accessSync(uploadPath);
+        // }catch(e){
+        //     // fs.mkdirSync('./public/image/');
+        //     fs.mkdirSync(uploadPath);
+        // }
 
         var storage = multer.diskStorage({
             destination: function(req, file, cb){
-                cb(null,'./public/image/');
+                cb(null,uploadPath);
             },
             filename: function(req,file,cb){
                 cb(null,currentTime+'-'+file.originalname);
